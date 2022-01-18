18af51c166d6e612acda72a178f18f5140667e84
aliwoto <aminnimaj@gmail.com>
2022-01-18 12:08:26 +0100

    Add more tests
    Signed-off-by: aliwoto <aminnimaj@gmail.com>

--- tests/test_client.py
+++ tests/test_client.py
@@ -3,8 +3,12 @@ from wotoplatform import WotoClient
 from wotoplatform.types.errors.serverErrors import NotModified
 from .myconfig import the_config
 
+__TEST_BIO_VALUE01__ = 'This is my test bio'
+__TEST_FIRST_NAME_VALUE01__ = 'ALiwoto'
+__TEST_LAST_NAME_VALUE01__ = 'lastname~'
+
 @pytest.mark.asyncio
-async def test_woto_client():
+async def test_woto_client01():
     client = WotoClient(
         the_config.username, 
         the_config.password, 
@@ -13,8 +17,28 @@ async def test_woto_client():
     )
     await client.start()
     print(await client.get_me())
+
     try:
-        print(await client.change_user_bio('hello, this a test'))
-    except NotModified:
-        print('bio is already that value')
+        assert await client.change_user_bio(__TEST_BIO_VALUE01__)
+    except NotModified: pass
+    
+    me = await client.get_me()
+    assert me.bio == __TEST_BIO_VALUE01__
+
+    try:
+        assert await client.change_names(
+            first_name=__TEST_FIRST_NAME_VALUE01__, 
+            last_name=__TEST_LAST_NAME_VALUE01__,
+        )
+    except NotModified: pass
+
+    me = await client.get_me()
+    assert me.first_name == __TEST_FIRST_NAME_VALUE01__
+    assert me.last_name == __TEST_LAST_NAME_VALUE01__
+
+    user_info = await client.get_user_info(the_config.username)
+    assert user_info.username == the_config.username
+    assert user_info.first_name == __TEST_FIRST_NAME_VALUE01__
+    assert user_info.last_name == __TEST_LAST_NAME_VALUE01__
+    assert user_info.bio == __TEST_BIO_VALUE01__
 
